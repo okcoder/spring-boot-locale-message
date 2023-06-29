e54c2422052a3806564d6207519aa1a43a370685
okcoder <okcoder@live.com>
2023-06-29 22:40:38 +0900

    create project via start.spring.io

---
+++ src/main/java/org/okcoder/sample/spring_boot_locale_message/SpringBootLocaleMessageApplication.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_boot_locale_message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLocaleMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLocaleMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/okcoder/sample/spring_boot_locale_message/SpringBootLocaleMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.okcoder.sample.spring_boot_locale_message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocaleMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
